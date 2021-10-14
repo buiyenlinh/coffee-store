b34844f5735fe4be4cde3e460912a52e0ceb5068
buiyenlinh <buiyenlinh1406@gmail.com>
2021-10-14 09:15:38 +0700

    menu

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Models\User;
+use App\Models\Role;
+use App\Models\User;
 
 class AdminController extends Controller
 {
+    /**
+    *    login page
+    */
     public function viewLogin() {
         return view('admin.login');
     }
+
+    /**
+     * Send login's data
+     */
 
     public function login(Request $request) {
         if (Auth::check()) {
@@ -39,7 +47,73 @@ class AdminController extends Controller
         return redirect()->route('home');
     }
 
+    /**
+     * Get user's level
+     * @param integer $id
+     * @return integer
+     */
+    private function getLevel($id = 0) {
+        if ($id == 0) {
+            $id = Auth::user()->role_id;
+        }
+        $role = Role::find($id);
+        return $role->id;
+    }
+
+    /**
+     * Get menu info
+     */
+    private function getMenu() {
+        $level = $this->getLevel();
+        $menu = [];
+        $menu[] = [
+            'link' => route('home'),
+            'title' => 'Trang chủ',
+            'icon' => 'fas fa-tachometer-alt'
+        ];
+
+        $menu[] = [
+            'link' => route('home'),
+            'title' => 'Danh sách bàn',
+            'icon' => 'fas fa-table'
+        ];
+
+        $menu[] = [
+            'link' => route('home'),
+            'title' => 'Sản phẩm',
+            'icon' => 'fas fa-coffee'
+        ];
+
+        $menu[] = [
+            'link' => route('home'),
+            'title' => 'Loại sản phẩm',
+            'icon' => 'fas fa-clipboard-list'
+        ];
+        return $menu;
+    }
+
+    /**
+     * Get info after login
+     */
+    private function getData() {
+        $user = Auth::user()->toArray();
+        $menu = $this->getMenu();
+        if (empty($user['avatar'])) {
+            $user['avatar'] = 'https://i.pinimg.com/736x/4e/db/ff/4edbff0d52c0d2f8e85fe0c0cc903993.jpg';
+        }
+        $data = [
+            'user' => $user,
+            'menu' => $menu
+        ];
+
+        return $data;
+    }
+
+    /**
+     * Admin page
+     */
     public function viewHome() {
-        return view('admin.home');
+        $data = $this->getData();
+        return view('admin.home', $data);
     }
 }
--- resources/views/admin/layout/adminLayout.blade.php
+++ resources/views/admin/layout/adminLayout.blade.php
@@ -4,9 +4,30 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="{{ asset('js/app.js') }}"></script>  
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="/css/admin.css">
   <title>{{ $title ?? 'Title your website' }}</title>
 </head>
 <body>
-  
+  <div class="wrapper">
+    <div class="main-sidebar">
+      <div class="sidebar">
+        <ul class="nav nav-pills flex-column" role="tablist">
+          @foreach ($menu as $_menu)
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="pill" href="{{ $_menu['link'] }}">
+              <em class="{{ $_menu['icon'] }}"></em>
+              {{ $_menu['title'] }}
+            </a>
+          </li>
+          @endforeach
+        </ul>
+      </div>
+    </div>
+    <div class="main-content">
+      @yield('content')
+    </div>
+  </div>
 </body>
 </html>
