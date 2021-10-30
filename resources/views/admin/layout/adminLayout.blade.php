fb5787d5413c71d84f9c3998feeb0b00e453d2fe
buiyenlinh <buiyenlinh1406@gmail.com>
2021-10-30 11:42:11 +0700

    create profile template

--- resources/views/admin/layout/adminLayout.blade.php
+++ resources/views/admin/layout/adminLayout.blade.php
@@ -5,9 +5,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="{{ asset('js/app.js') }}"></script>  
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="/css/admin.css">
+  <script src="{{ asset('js/app.js') }}"></script>  
   <title>Quản trị - {{ $title ?? 'Your site title' }}</title>
 </head>
 <body>
